5913229c066952b474524b9eb2e0bf16b568abc9
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2026-06-22 12:32:48 +0600

    feat: update Prisma schema and add initial migration files

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
-// import config from "./config";
+import config from "./config";
+
 // import { userRoutes } from "./modules/user/user.route";
 
 
 
 const app : Application = express();
 
-// app.use(cors({
-//     origin : config.app_url,
-//     credentials : true,
-// }))
+app.use(cors({
+    origin : config.app_url,
+    credentials : true,
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
