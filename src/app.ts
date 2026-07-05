9f7d85b97242955cb90b8d2ee9fee409a0023086
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2026-07-05 21:56:13 +0600

    feat: add global error handling and not found middleware; refactor catchAsync utility

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,57 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import config from "./config";
 import { authRoutes } from "./modules/auth/auth.routes";
 import { userRoutes } from "./modules/user/user.route";
 import { postRoutes } from "./modules/post/post.route";
 import { commentRoutes } from "./modules/comment/comment.route";
+import path from "node:path";
+import { notFound } from "./middlewares/notFound";
+import { globalErrorHandler } from "./middlewares/globalErrorHandlar";
 
+const app: Application = express();
 
-
-const app : Application = express();
-
-app.use(cors({
-    origin : config.app_url,
-    credentials : true,
-}))
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-app.get("/",(req : Request, res : Response) => {
-    res.send("Hello, World!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
 });
 
 // app.post()
 
-app.use("/api/users", userRoutes)
-app.use("/api/auth", authRoutes)
-app.use("/api/posts",postRoutes)
-app.use("/api/comments",commentRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+
+// app.use((req: Request, res: Response) => {
+//   res.status(404).json({
+//     message: "Route not found",
+//     path: req.originalUrl,
+//     date: Date(),
+//   });
+// });
+
+app.use(notFound);
+// app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+//   res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//     success: false,
+//     statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//     message: err.message,
+//     error: err.stake,
+//   });
+// });
+
+app.use(globalErrorHandler);
 
 export default app;
